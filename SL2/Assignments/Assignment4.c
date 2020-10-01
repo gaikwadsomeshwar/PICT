2caeafadb8f76851bdd2d6fd931b63a3f0b5cb23
DARkNIGHT <gaikwadsomeshwar6@gmail.com>
2020-10-01 13:02:27 +0530

    updating Ass4 of SL2

--- SL2/Assignments/Assignment4.c
+++ SL2/Assignments/Assignment4.c
@@ -20,24 +20,31 @@ int buffer[BufferSize];
 int main() {
 
   int res,i,sizeP,sizeC;
+  pthread_t *threadP,*threadC;
 
   printf("\nNumber of Producers: ");
   scanf("%d",&sizeP);
 
-  printf("\nNumber of Consumers: ");
+  printf("Number of Consumers: ");
   scanf("%d",&sizeC);
   printf("\n");
 
-  pthread_t threadP[sizeP],threadC[sizeC];
+  threadP=(pthread_t*)malloc(sizeof(pthread_t)*sizeP);
+  threadC=(pthread_t*)malloc(sizeof(pthread_t)*sizeC);
+  int pno[sizeP],cno[sizeC];
 
   full=sem_open("/fullSem", O_CREAT, 0644, 0);
   empty=sem_open("/emptySem", O_CREAT, 0644, BufferSize);
-  mutex=sem_open("/mutexSem", O_CREAT, 0644, 1);
+  mutex=sem_open("/mutexSem", O_CREAT, 0644, 0);
 
-  for(i=0;i<sizeP;i++)
-    res=pthread_create(&threadP[i], NULL, producer, NULL);
-  for(i=0;i<sizeC;i++)
-    res=pthread_create(&threadC[i], NULL, consumer, NULL);
+  for(i=0;i<sizeP;i++) {
+    pno[i]=i+1;
+    res=pthread_create(&threadP[i], NULL, producer, &pno[i]);
+  }
+  for(i=0;i<sizeC;i++) {
+    cno[i]=i+1;
+    res=pthread_create(&threadC[i], NULL, consumer, &cno[i]);
+  }
   for(i=0;i<sizeP;i++)
     res=pthread_join(threadP[i], NULL);
   for(i=0;i<sizeC;i++)
@@ -53,16 +60,19 @@ void *producer(void *arg) {
 
   while(1) {
     int item=rand();
+
     sem_wait(empty);
     sem_wait(mutex);
-    buffer[in]=item%10;
+
+    buffer[in]=item%100;
+    int pos=in;
     in=(in+1)%BufferSize;
 
-    printf("[ ");
+    printf("Thread ID: %d. Producer: %d",(int)pthread_self()%100,*(int*)arg);
+    printf("\nProduced item %d at %d position\nBuffer:[ ",item%100,pos);
     for(int i=0;i<BufferSize;i++)
       printf("%d ",buffer[i]);
-    printf("]");
-    printf("\nProduced: %d\n\n",item%10);
+    printf("]\n\n");
 
     sem_post(mutex);
     sem_post(full);
@@ -74,16 +84,23 @@ void *producer(void *arg) {
 void *consumer(void *arg) {
 
   while(1) {
+
     sem_wait(full);
     sem_wait(mutex);
+
     int item=buffer[out];
+    int pos=out;
     out=(out+1)%BufferSize;
 
-    printf("[ ");
+    printf("Thread ID: %d. Consumer: %d",(int)pthread_self()%100,*(int*)arg);
+    printf("\nConsuming item %d from %d position\nBuffer:[ ",item,pos);
     for(int i=0;i<BufferSize;i++)
       printf("%d ",buffer[i]);
-    printf("]");
-    printf("\nConsuming: %d\n\n",item);
+    printf("]->[ ");
+    buffer[pos]=0;
+    for(int i=0;i<BufferSize;i++)
+      printf("%d ",buffer[i]);
+    printf("]\n\n");
 
     sem_post(mutex);
     sem_post(empty);
