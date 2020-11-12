8995825a8bc08c7d5dcdd3b88615fd92da0fe984
DARkNIGHT <gaikwadsomeshwar6@gmail.com>
2020-11-12 13:40:33 +0530

    SL2 Update

--- SL2/Assignments/Assignment7/Assignment7a.c
+++ SL2/Assignments/Assignment7/Assignment7a.c
@@ -5,28 +5,53 @@
 
 int main() {
 
-  int file_pipes1[2],file_pipes2[2],pid;
-  const char data1[]="Hello Person1",data2[]="Hello Person2";
-  char buffer[20];
-  pipe(file_pipes1);
-  pipe(file_pipes2);
+  int file_pipes1[2]; //Parent Pipe
+  int file_pipes2[2]; //Child Pipe
+  int pid,count=0;
+  char fileContent[999],filename[]="record.txt",msg[]="This is a new message.";
+  FILE *fptr;
+  pipe(file_pipes1); //One Way Communication from Parent to Child
+  pipe(file_pipes2); //One Way Communication from Child to Parent
+
   pid=fork();
 
   if(pid==0) { //Child Process
-    close(file_pipes1[1]); // Close the unwanted pipe1 write side
-    close(file_pipes2[0]); // Close the unwanted pipe2 read side
-    read(file_pipes1[0],buffer,sizeof(buffer));
-    printf("In Child : Reading from pipe 1 : Message is %s\n\n",buffer);
-    printf("In Child : Writing to pipe   2 : Message is %s\n",data2);
-    write(file_pipes2[1],data2,sizeof(data2));
+
+    close(file_pipes1[1]); // Close the unwanted write side of parent
+    close(file_pipes2[0]); // Close the unwanted read side of child
+
+    fptr=fopen("Record.txt","a");
+    fprintf(fptr,"%s\n",msg);
+    fclose(fptr);
+
+    int x=write(file_pipes2[1],msg,sizeof(msg));
+    printf("\nIn Child  (id %d): Writing to Pipe 2\n\n%s\n(Number of characters written: %d)\n",getpid(),msg,x);
+    close(file_pipes2[1]);
+
+    read(file_pipes1[0],fileContent,sizeof(fileContent));
+    printf("\nIn Child  (id %d): Reading from Pipe 2\n\n%s\n",getpid(),fileContent);
+    close(file_pipes2[0]);
   }
   else { //Parent Process
-    close(file_pipes1[0]); // Close the unwanted pipe1 read side
-    close(file_pipes2[1]); // Close the unwanted pipe2 write side
-    printf("\nIn Parent: Writing to pipe   1 : Message is %s\n",data1);
-    write(file_pipes1[1],data1,sizeof(data1));
-    read(file_pipes2[0],buffer,sizeof(buffer));
-    printf("In Parent: Reading from pipe 2 : Message is %s\n\n",buffer);
+
+    close(file_pipes1[0]); // Close the unwanted read side of parent
+    close(file_pipes2[1]); // Close the unwanted write side child
+
+    //Reading from child pipe
+    read(file_pipes2[0],fileContent,sizeof(fileContent));
+    printf("\nIn Parent (id %d): Reading from Pipe 2\n\n%s\n",getpid(),fileContent);
+
+    fptr=fopen("Record.txt","r");
+    while(!feof(fptr)) {
+			fileContent[count++] = fgetc(fptr);
+		}
+    fclose(fptr);
+    close(file_pipes2[0]); //Close the unwanted read side of child
+
+    //writing to parent pipe
+    write(file_pipes1[1],fileContent,sizeof(fileContent));
+    printf("\nIn Parent (id %d): Writing to Pipe 1\n\n%s\n",getpid(),fileContent);
+    close(file_pipes1[1]); // Close the unwanted write side of parent
   }
   return 0;
 }
