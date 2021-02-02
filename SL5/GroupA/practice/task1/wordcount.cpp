ec71c3a65fc5e00cecaf8ce796f4d3ae21942425
PICT <gaikwadsomeshwar6@gmail.com>
2021-02-03 00:17:50 +0530

    Update Opcode Task

--- SL5/GroupA/practice/task1/wordcount.cpp
+++ SL5/GroupA/practice/task1/wordcount.cpp
@@ -1,61 +1,71 @@
 #include <iostream>
-# include <fstream>
+#include <fstream>
+#include <iterator>
+#include <map>
 using namespace std;
 
 int main() {
+
+  map<string, string> opcode;
+
+  //sample opcode table
+  opcode["START"] = "00";
+  opcode["ADD"] = "01";
+  opcode["SUB"] = "02";
+  opcode["DIV"] = "03";
+  opcode["MOVER"] = "04";
+  opcode["MOVEM"] = "05";
+  opcode["JMP"] = "06";
+  opcode["STOP"] = "07";
+  opcode["END"] = "08";
 
   cout<< endl;
   ifstream fin;
   string line;
 
   fin.open("file.txt");
+  int i = 0;
 
-  while(fin) {
+  //Separating individual words
+  while(getline(fin, line)) {
 
-    getline(fin, line);
     int count = 0;
     string word = "";
 
-    string firstword = line.substr(0, line.find(" "));
-    count ++;
-    cout<< firstword;
+    cout<< "========================"<< endl;
+    cout<< ++i<< "] "<< line<< endl<< endl;
+    cout<< "+-+-+-+-+-+-+-+"<< endl;
+    cout<< "Opcodes:"<< endl<< endl;
+    for(auto ch: line) {
 
-    ifstream opcode;
-    opcode.open("opcode.txt");
-
-    while(opcode) {
-
-      string mne;
-      getline(opcode, mne);
-
-      if(firstword.compare(mne.substr(0, line.find(" "))) == 0) {
-
-        int firstSpace = mne.find(" ");
-
-        cout<< " (Opcode: "<< mne.substr(firstSpace + 1)<< ")";
-      }
-    }
-
-    cout<< endl;
-    opcode.close();
-
-    for(int x = line.find(" ") + 1; x < line.length(); x++) {
-
-      if(line.at(x) == ' ') {
-        cout<< word<< endl;
-        word = "";
+      if(ch == ' ') {
         count++;
+        //finding the word in the opcode table
+        map<string, string>::iterator itr = opcode.find(word);
+        if(itr != opcode.end()) {
+          cout<< itr->first<< ": "<< itr->second<< endl;
+        }
+        word = "";
       }
       else {
-        word = word + line.at(x);
+        word = word + ch;
       }
     }
-    cout<< word<< endl;
-    count++;
 
-    cout<< "Count of Words: "<< count<< endl<< endl;
+    //Last Word Handling
+    if(word.length() > 0) {
+      count++;
+      map<string, string>::iterator itr = opcode.find(word);
+      if(itr != opcode.end()) {
+        cout<< itr->first<< ": "<< itr->second<< endl;
+      }
+      word = "";
+    }
+
+    cout<< "+-+-+-+-+-+-+-+"<< endl;
+    cout<< endl<< "Number of Words: "<< count <<endl;
   }
-
+  cout<< "========================"<< endl;
   fin.close();
   return 0;
 }
